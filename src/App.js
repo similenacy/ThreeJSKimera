7f943e42d01c42ae9423b98d623b99a2c22accb6
John Doe <similenacy@gmail.com>
2022-09-28 16:27:52 +0100

    first commit: project created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import { Suspense, useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import './styles.css'
 
-function App() {
+function MainScene(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+
+
+ 
+  // Return the view, these are regular Threejs elements expressed in JSX
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <group>
+      <gridHelper args={[20,20]}  />
+      <OrbitControls target={[0,0.35,0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3,2,5]} />
+
+    <mesh
+      {...props}
+      ref={ref}
+      scale={1}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color={"green"}/>
+    </mesh>
+    </group>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    
+      <Suspense fallback={null}>
+        <Canvas shadows>
+          <MainScene />
+        </Canvas>
+      </Suspense>
+  );
+}
